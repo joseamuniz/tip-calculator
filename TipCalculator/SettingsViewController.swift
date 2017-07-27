89d5707b79f2ef7c0030736ac2cee29558e49bcf
Jose Muniz <jose@logichub.com>
2017-07-27 08:48:47 -0700

    create first version of TipCalculator

---
+++ TipCalculator/SettingsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SettingsViewController.swift
+//  TipCalculator
+//
+//  Created by Jose Muniz on 7/26/17.
+//  Copyright © 2017 Jose Muniz. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+//    @IBOutlet weak var minTip: UITextField!
+    @IBOutlet weak var medTip: UITextField!
+    @IBOutlet weak var maxTip: UITextField!
+    @IBOutlet weak var minTip: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let defaults = UserDefaults.standard
+        minTip.text = defaults.string(forKey: "minTip") ?? "0.15"
+        medTip.text = defaults.string(forKey: "medTip") ?? "0.20"
+        maxTip.text = defaults.string(forKey: "maxTip") ?? "0.25"
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func tipEdited(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(minTip.text, forKey: "minTip")
+        defaults.set(medTip.text, forKey: "medTip")
+        defaults.set(maxTip.text, forKey: "maxTip")
+    }
+
+
+
+}
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipPercent: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let defaults = UserDefaults.standard
+        tipPercent.setTitle(defaults.string(forKey: "maxTip") ?? "0.15", forSegmentAt: 2)
+        tipPercent.setTitle(defaults.string(forKey: "medTip") ?? "0.20", forSegmentAt: 1)
+        tipPercent.setTitle(defaults.string(forKey: "minTip") ?? "0.25", forSegmentAt: 0)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
 
+    @IBAction func billAdded(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        let tip = Double(tipPercent.titleForSegment(at: tipPercent.selectedSegmentIndex)!) ?? 0
+        
+        tipLabel.text = String(format: "$%.2f", (bill * tip))
+        totalLabel.text = String(format: "$%.2f", (bill * (1 + tip)))
+    }
 
 }
-
